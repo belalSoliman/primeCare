690b097bdd7e5255d477082cdda4158aa950a6be
belal_soliman <belalsoliman8@gmail.com>
2024-08-24 18:37:51 +0300

    create the project and make it work only in portrait mode

---
+++ android/app/src/main/kotlin/com/example/pharnacy_trust/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pharnacy_trust
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pharnacy_trust/pharmacy_entry_point.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (_) => runApp(const PharmacyEntryPoint()),
+  );
+}
