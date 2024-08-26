667d244c8d3e7e8886274b3b18185d2de6fed152
belal_soliman <belalsoliman8@gmail.com>
2024-08-26 19:16:30 +0300

    fixed some issues on the web view

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pharnacy_trust/pharmacy_entry_point.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
-    (_) => runApp(const PharmacyEntryPoint()),
-  );
+
+  // Only lock orientation on mobile platforms
+  if (!kIsWeb) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+      (_) => runApp(const PharmacyEntryPoint()),
+    );
+  } else {
+    runApp(const PharmacyEntryPoint());
+  }
 }
