9035b7ced6768081b0c21758d12b93440517ef4d
belal_soliman <belalsoliman8@gmail.com>
2024-08-26 14:52:01 +0300

    test the category item

--- lib/widget/category_page_widgets/category_item.dart
+++ lib/widget/category_page_widgets/category_item.dart
@@ -5,8 +5,31 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Category Item'),
+    return Column(
+      mainAxisSize: MainAxisSize
+          .min, // Ensure the column only takes up as much space as needed
+      children: [
+        Container(
+          margin: const EdgeInsets.all(10),
+          height: MediaQuery.of(context).size.width * 0.4,
+          width: MediaQuery.of(context).size.width * 0.4,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                "assets/category assets/sushi (1).png",
+              ),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        const Text(
+          "Sushi",
+          style: TextStyle(
+            fontSize: 16, // Adjust the font size as needed
+            fontWeight: FontWeight.bold, // Optional: Make the text bold
+          ),
+        ),
+      ],
     );
   }
 }
